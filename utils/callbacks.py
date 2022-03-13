e87dba0402bc88108d4e18451f178ca60ff6a2dc
Bubbliiiing <3323290568@qq.com>
2022-03-14 01:58:47 +0800

    update a lot

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,39 +1,32 @@
 import os
 
 import keras
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 import scipy.signal
 from keras import backend as K
 
-
-class ExponentDecayScheduler(keras.callbacks.Callback):
-    def __init__(self, decay_rate, verbose=0):
-        super(ExponentDecayScheduler, self).__init__()
-        self.decay_rate = decay_rate
-        self.verbose = verbose
-
-    def on_epoch_end(self, batch, logs=None):
-        lr = K.get_value(self.model.optimizer.lr) * self.decay_rate
-        K.set_value(self.model.optimizer.lr, lr)
-        if self.verbose > 0:
-            print('Setting learning rate to %s.' % lr)
-
 class LossHistory(keras.callbacks.Callback):
     def __init__(self, log_dir):
-        import datetime
-        curr_time       = datetime.datetime.now()
-        time_str        = datetime.datetime.strftime(curr_time,'%Y_%m_%d_%H_%M_%S')
         self.log_dir    = log_dir
-        self.time_str   = time_str
-        self.save_path  = os.path.join(self.log_dir, "loss_" + str(self.time_str))  
         self.losses     = []
+        self.val_loss   = []
         
-        os.makedirs(self.save_path)
+        os.makedirs(self.log_dir)
 
-    def on_epoch_end(self, batch, logs={}):
+    def on_epoch_end(self, epoch, logs={}):
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+
         self.losses.append(logs.get('loss'))
-        with open(os.path.join(self.save_path, "epoch_loss_" + str(self.time_str) + ".txt"), 'a') as f:
+        self.val_loss.append(logs.get('val_loss'))
+        
+        with open(os.path.join(self.log_dir, "epoch_loss.txt"), 'a') as f:
             f.write(str(logs.get('loss')))
+            f.write("\n")
+        with open(os.path.join(self.log_dir, "epoch_val_loss.txt"), 'a') as f:
+            f.write(str(logs.get('val_loss')))
             f.write("\n")
         self.loss_plot()
 
@@ -42,6 +35,7 @@ class LossHistory(keras.callbacks.Callback):
 
         plt.figure()
         plt.plot(iters, self.losses, 'red', linewidth = 2, label='train loss')
+        plt.plot(iters, self.val_loss, 'coral', linewidth = 2, label='val loss')
         try:
             if len(self.losses) < 25:
                 num = 5
@@ -49,6 +43,7 @@ class LossHistory(keras.callbacks.Callback):
                 num = 15
             
             plt.plot(iters, scipy.signal.savgol_filter(self.losses, num, 3), 'green', linestyle = '--', linewidth = 2, label='smooth train loss')
+            plt.plot(iters, scipy.signal.savgol_filter(self.val_loss, num, 3), '#8B4513', linestyle = '--', linewidth = 2, label='smooth val loss')
         except:
             pass
 
@@ -58,7 +53,22 @@ class LossHistory(keras.callbacks.Callback):
         plt.title('A Loss Curve')
         plt.legend(loc="upper right")
 
-        plt.savefig(os.path.join(self.save_path, "epoch_loss_" + str(self.time_str) + ".png"))
+        plt.savefig(os.path.join(self.log_dir, "epoch_loss.png"))
 
         plt.cla()
         plt.close("all")
+
+class ExponentDecayScheduler(keras.callbacks.Callback):
+    def __init__(self,
+                 decay_rate,
+                 verbose=0):
+        super(ExponentDecayScheduler, self).__init__()
+        self.decay_rate         = decay_rate
+        self.verbose            = verbose
+        self.learning_rates     = []
+
+    def on_epoch_end(self, batch, logs=None):
+        learning_rate = K.get_value(self.model.optimizer.lr) * self.decay_rate
+        K.set_value(self.model.optimizer.lr, learning_rate)
+        if self.verbose > 0:
+            print('Setting learning rate to %s.' % (learning_rate))
